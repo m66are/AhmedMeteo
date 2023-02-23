04af55593c10728263f548b6d2a4716325b7c11a
Montassare Zekri <34745302+m66are@users.noreply.github.com>
2023-02-23 22:47:29 +0100

    Initial commit

---
+++ src/redux/Actions.jsx
@@ -0,0 +1,30 @@
+import { GET_WEATHER } from "./Constants";
+import axios from "axios";
+import { BASE_URL } from "../components/ApiConstants";
+import { toast } from "react-toastify";
+export const GetWeatherDetails = (location = "hyderabad") => async dispatch => {
+  dispatch({ type: GET_WEATHER.PENDING });
+  axios
+    .get(BASE_URL, {
+      params: {
+        q: location,
+        units: "Metric",
+        lang: "Fr"
+      }
+    })
+    .then(response =>
+      dispatch({ type: GET_WEATHER.SUCCESS, payload: response.data })
+    )
+    .catch(err => {
+      console.log(err.response, err);
+      toast.error(err.response.data.message, {
+        position: "bottom-center",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: false,
+        draggable: false
+      });
+      dispatch({ type: GET_WEATHER.REJECTED, payload: err.response });
+    });
+};
